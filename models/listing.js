aad9477f31821cdbc14ec513082a6e0eee83b0de
Kashish-002 <aryakashish02@gmail.com>
2025-08-27 14:17:02 +0530

    Set a default image for listing

--- models/listing.js
+++ models/listing.js
@@ -10,8 +10,14 @@ const listingSchema=new mongoose.Schema({
     },
     image:{     
     filename: String,
-    url: String,
-    // set:(v)=>v===""?"default":v,
+    url:{
+        type:String,
+        default:"https://plus.unsplash.com/premium_photo-1678286771657-cf22aa97faf0?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+
+    set:(v)=>
+        v ===""
+    ? "https://plus.unsplash.com/premium_photo-1678286771657-cf22aa97faf0?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D":v,
+    },
     },
 
     price:{
