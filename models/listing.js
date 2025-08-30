fe139cdccf519ce542e9c0c8e63de1630b65d3dd
Kashish-002 <aryakashish02@gmail.com>
2025-08-30 14:41:22 +0530

    Add review in the listing

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,6 @@
 const mongoose=require("mongoose");
-
+const {Schema}=mongoose;
+const Review=require("./reviews.js")
 const listingSchema=new mongoose.Schema({
     title:{
         type:String,
@@ -29,8 +30,20 @@ const listingSchema=new mongoose.Schema({
     },
     country:{
         type:String,
+    },
+    reviews:[
+        {
+        type:Schema.Types.ObjectId,
+        ref:"Review",
     }
+],
 
+});
+
+listingSchema.post("findOneAndDelete",async(listing)=>{
+    if(listing){
+         await Review.deleteMany({_id:{$in:listing.reviews}});
+    }   
 });
 
 const Listing=mongoose.model("Listing",listingSchema);
