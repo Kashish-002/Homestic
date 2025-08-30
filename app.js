97e55efe02100646cfcdc31372baef24717793d9
Kashish-002 <aryakashish02@gmail.com>
2025-08-30 14:45:00 +0530

    review routes along with delete review routes are handled

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const methodOverride=require("method-override");
 const ejsMate=require("ejs-mate");
 const ExpressError=require("./utils/ExpressError.js");
 const wrapAsync=require("./utils/wrapAsync.js");
-const {listingSchema}=require("./schema.js");
+const {listingSchema, reviewSchema}=require("./schema.js");
+const Review=require("./models/reviews.js");
 
 app.set("view engine", "ejs");
 app.set("views",path.join(__dirname,"views"));
@@ -36,6 +37,17 @@ const validateListing=(req,res,next)=>{
     }
 };
 
+const validateReview=(req,res,next)=>{
+    let {error}=reviewSchema.validate(req.body);
+    if(error){
+        let errMsg=error.details.map((el)=>el.message).join(",");
+        throw new ExpressError(400,error);
+    }else{
+        next();
+    }
+};
+
+
 //index route
 app.get("/listings", wrapAsync(async (req,res)=>{
     const allListings=await Listing.find({});
@@ -57,7 +69,7 @@ app.post("/listings",validateListing ,wrapAsync(async (req,res)=>{
 //show route
 app.get("/listings/:id",wrapAsync(async (req,res)=>{
     let {id}=req.params;
-    const listings=await Listing.findById(id);
+    const listings=await Listing.findById(id).populate("reviews");
     res.render("listings/show.ejs",{listings});
 }));
 
@@ -81,6 +93,24 @@ app.delete("/listings/:id",wrapAsync(async (req,res)=>{
     res.redirect("/listings");
 }));
 
+app.post("/listings/:id/reviews",validateReview ,wrapAsync (async (req,res)=>{
+    let listing=await Listing.findById(req.params.id);
+    let newReview=new Review(req.body.review);
+
+    listing.reviews.push(newReview);
+    await newReview.save();
+    await listing.save();
+    res.redirect(`/listings/${listing._id}`);
+}));
+
+//Delete review route
+app.delete("/listings/:id/reviews/:reviewId",wrapAsync(async(req,res)=>{
+    let {id,reviewId}=req.params;
+    await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/listings/${id}`);
+}));
+
 // app.get("/testlisting",(req,res)=>{
 //     res.send("working");
 // });
@@ -91,7 +121,7 @@ app.delete("/listings/:id",wrapAsync(async (req,res)=>{
 
 app.use((err,req,res,next)=>{
     let{statusCode=500,message="Something went wrong"}=err;
-   
+
     res.render("listings/error.ejs",{message});
 });
 
