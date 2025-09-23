b3be712e0666e7ca89f0a2bcaf5fc502f80ecb4b
Kashish-002 <aryakashish02@gmail.com>
2025-09-23 22:28:40 +0530

    Add passport import and and session for authentication

--- app.js
+++ app.js
@@ -7,10 +7,13 @@ const ejsMate=require("ejs-mate");
 const ExpressError=require("./utils/ExpressError.js");
 const session=require("express-session");
 const flash=require("connect-flash");
+const passport=require("passport");
+const LocalStrategy=require("passport-local");
+const User=require("./models/user.js");
 
 const listings=require("./routes/listings.js");
 const review=require("./routes/review.js");
-
+const user=require("./routes/user.js");
 
 main()
 .then(()=>{
@@ -43,6 +46,15 @@ const sessionOptions={
 app.use(session(sessionOptions));
 app.use(flash());
 
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req,res,next)=>{
     res.locals.success=req.flash("success");
     res.locals.error=req.flash("error");
@@ -51,6 +63,7 @@ app.use((req,res,next)=>{
 
 app.use("/listings",listings);
 app.use("/listings/:id/reviews",review);
+app.use("/",user)
 
 // app.all("*",(req,res,next)=>{
 //     next(new ExpressError(404,"Page not found"));
