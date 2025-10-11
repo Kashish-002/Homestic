f9189150a077f1264a8553c194a2f537af20d22d
Kashish-002 <aryakashish02@gmail.com>
2025-10-11 23:48:39 +0530

    Add owner id for listings

--- init/index.js
+++ init/index.js
@@ -14,6 +14,8 @@ async function main(){
 
 const initDB=async ()=>{
     await Listing.deleteMany({});
+    initdata.data= initdata.data.map((obj)=>({...obj, owner: "68d2cefe68ce6519d48d7904",
+    }));
     await Listing.insertMany(initdata.data);
     console.log("Done"); 
 }
